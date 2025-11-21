1a59c1be10fb78ee03e8f3e3294f1c71a1ff18da
TheGvard <161474189+TheGvard@users.noreply.github.com>
2025-11-21 10:38:18 +0300

    Create lab5.cpp

---
+++ lab5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<cmath>
+#include <locale>
+
+int main() {
+	int n, m;
+	setlocale(LC_ALL, "rus");
+	std::cout << "Проверка элементов столбца на нулевое значение\n";
+	std::cout << "Кол-во строк: "; std::cin >> n;
+	std::cout << "Кол-во столбцов: "; std::cin >> m;
+	std::cout << "Заполните матрицу (граница строки обозначена -----)";
+	double **matrix = new double* [n];
+	for (int i = 0; i < n; i++) {
+		matrix[i] = new double[m];
+		for (int j = 0; j < m; j++) {
+			std::cin >> matrix[i][j];
+		}
+		std::cout << "-----";
+	}
+
+	double* result = new double[m];
+	for (int i = 0; i < m; i++) {
+		result[i] = 0;
+		for (int j = 0; j < n; j++) {
+			if (matrix[i][j] == 0) {
+				result[i] = 1;
+			}
+		}
+		std::cout << result[i] << " ";
+	}
+	std::cout << "\n";
+	for (int i = 0; i < n; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+	delete[] result;
+	return 0;
+}
