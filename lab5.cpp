1d66d55b960d6b234c549078bacd69bf56ebe47f
TheGvard <161474189+TheGvard@users.noreply.github.com>
2025-11-21 10:50:18 +0300

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include<cmath>
 #include <locale>
 
@@ -19,9 +20,9 @@ int main() {
 	}
 
 	double* result = new double[m];
-	for (int i = 0; i < m; i++) {
+	for int j = 0; j < n; j++) {
 		result[i] = 0;
-		for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m; i++)({
 			if (matrix[i][j] == 0) {
 				result[i] = 1;
 			}
