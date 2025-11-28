c62759e3b4c82476add3d00db66e2115a245f591
TheGvard <161474189+TheGvard@users.noreply.github.com>
2025-11-28 10:53:52 +0300

    Update lab5-custom.cpp

--- lab5-custom.cpp
+++ lab5-custom.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <vector>
 #include<cmath>
 #include <locale>
+#include <vector>
 
 int main() {
-	// вектора имеют встроенный деструктор, могут изменяться в рантайме и поддерживают кучу функций и вообще удобнее в написании
 	int n, m;
 	setlocale(LC_ALL, "rus");
 	std::cout << "Проверка элементов столбца на нулевое значение\n";
-	std::cout << "Кол-во строк: "; std::cin >> n;
 	std::cout << "Кол-во столбцов: "; std::cin >> m;
-	std::cout << "Заполните матрицу (граница строки обозначена -----)";
+	std::cout << "Кол-во строк: "; std::cin >> n;
+	std::cout << "Заполните матрицу (граница строки обозначена -----)\n";
 	std::vector<std::vector<double>> matrix(n, std::vector<double>(m, 0));
 	for (int j = 0; j < m; j++) {
 		for (int i = 0; i < n; i++) {
 			std::cin >> matrix[i][j];
 		}
-		std::cout << "-----";
+		std::cout << "-----\n";
 	}
+	std::cout << "\nВывод: ";
 	std::vector<double> result(m, 0);
-	std::cout << "\n";
-	for (int j = 0; j < n; j++) {;
-		for (int i = 0; i < m; i++) {
-			if (matrix[i][j] == 0) {
-				result[j] = 1;
-			}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (matrix[j][i] != 0) result[i] = 1;
 		}
-		std::cout << result[j] << " ";
+		std::cout << result[i];
 	}
 	return 0;
 }
